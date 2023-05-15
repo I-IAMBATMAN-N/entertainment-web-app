28f715ee9703d2923532ac7f560e353410d6daf2
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-05-16 00:10:43 +0200

    [CREATE]Fill TV Shows function

--- script.js
+++ script.js
@@ -637,6 +637,8 @@ navItems.forEach((navItem, index) => {
     console.log(index);
     if (index === 1) {
       fillMovies();
+    } else if (index === 2) {
+      fillTVShows();
     }
   });
 });
@@ -646,6 +648,50 @@ function fillMovies() {
     if (movie.category === "Movie") {
       // console.log(movie.category);
       moviesContainerSection.innerHTML += `
+      <figure class="movie-item sm">
+      <div class="bookmark">
+        <svg
+          width="12"
+          height="14"
+          xmlns="http://www.w3.org/2000/svg"
+          class="bookmark-icon"
+        >
+          <path
+            d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
+          />
+        </svg>
+      </div>
+      <img
+        src="${selectImg(movie)}"
+        alt=""
+        class="movie-img"
+      />
+      <figcaption>
+        <ul class="movie-properties">
+          <li class="movie-property">
+            <p>${movie.year}</p>
+          </li>
+          <li class="movie-property">
+            <img src="assets/icon-category-movie.svg" alt="" />
+            <p>${movie.category}</p>
+          </li>
+          <li class="movie-property">
+            <p>${movie.rating}</p>
+          </li>
+        </ul>
+        <h3 class="heading-medium">${movie.title}</h3>
+      </figcaption>
+    </figure>
+        `;
+    }
+  });
+}
+function fillTVShows() {
+  movies.forEach((movie) => {
+    // console.log(movie.category);
+    if (movie.category === "TV Series") {
+      // console.log(movie.category);
+      tvShowsSection.innerHTML += `
       <figure class="movie-item sm">
       <div class="bookmark">
         <svg
