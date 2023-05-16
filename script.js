f8165f586b76b3ca8b85d53fdc5c60c5baf07c9d
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-05-16 02:52:03 +0200

    [UPDATE]Search Basic function
    - function can now search in Bookmarked section

--- script.js
+++ script.js
@@ -836,10 +836,15 @@ mainInput.addEventListener("input", function () {
           }
         });
         search.classList.add("active");
-      } else if (index === 1 || index === 2 || index === 3) {
+      } else if (index === 1 || index === 2) {
         // console.log(containers[index].children[0].children[1]);
         containers[index].children[0].children[1].innerHTML = ``;
         mainIndex = index;
+      } else if (index === 3) {
+        containers[index].children[0].children[1].innerHTML = ``;
+        containers[index].children[1].children[1].innerHTML = ``;
+        mainIndex = index;
+        // containers[index].children[0].children[1].innerHTML = ``;
       }
     }
   });
@@ -962,6 +967,80 @@ mainInput.addEventListener("input", function () {
           <h3 class="heading-medium">${movie.title}</h3>
         </figcaption>
       </figure>`;
+        }
+      } else if (mainIndex === 3) {
+        if (movie.isBookmarked) {
+          if (movie.category === "Movie") {
+            bookmarkedSectionMovies.innerHTML += `<figure class="movie-item sm">
+            <div class="bookmark ${movie.isBookmarked ? "active" : ""}">
+            <svg
+              width="12"
+              height="14"
+              xmlns="http://www.w3.org/2000/svg"
+              class="bookmark-icon"
+            >
+              <path
+                d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
+              />
+            </svg>
+          </div>
+          <img
+            src="${selectImg(movie)}"
+            alt=""
+            class="movie-img"
+          />
+          <figcaption>
+            <ul class="movie-properties">
+              <li class="movie-property">
+                <p>${movie.year}</p>
+              </li>
+              <li class="movie-property">
+                <img src="assets/icon-category-movie.svg" alt="" />
+                <p>${movie.category}</p>
+              </li>
+              <li class="movie-property">
+                <p>${movie.rating}</p>
+              </li>
+            </ul>
+            <h3 class="heading-medium">${movie.title}</h3>
+          </figcaption>
+        </figure>`;
+          } else if (movie.category === "TV Series") {
+            bookmarkedSectionTVShows.innerHTML += `<figure class="movie-item sm">
+            <div class="bookmark ${movie.isBookmarked ? "active" : ""}">
+            <svg
+              width="12"
+              height="14"
+              xmlns="http://www.w3.org/2000/svg"
+              class="bookmark-icon"
+            >
+              <path
+                d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
+              />
+            </svg>
+          </div>
+          <img
+            src="${selectImg(movie)}"
+            alt=""
+            class="movie-img"
+          />
+          <figcaption>
+            <ul class="movie-properties">
+              <li class="movie-property">
+                <p>${movie.year}</p>
+              </li>
+              <li class="movie-property">
+                <img src="assets/icon-category-movie.svg" alt="" />
+                <p>${movie.category}</p>
+              </li>
+              <li class="movie-property">
+                <p>${movie.rating}</p>
+              </li>
+            </ul>
+            <h3 class="heading-medium">${movie.title}</h3>
+          </figcaption>
+        </figure>`;
+          }
         }
       }
     }
