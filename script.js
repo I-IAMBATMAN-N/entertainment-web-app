becc57545a5e7fc52d7a5b47909008f51d4c6557
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-06-14 19:42:02 +0200

    [FIX]bookmark icon styling
    - changed source from svg to JS module + custom element
    
    script.js,
    index.html,
    style.css
    - functional adjustment to new icon

--- script.js
+++ script.js
@@ -501,14 +501,26 @@ function bookmarkCheck() {
       //
       if (event.target.closest(".bookmark")) {
         //
+        const bookmarkIcons = event.target
+          .closest(".bookmark")
+          .querySelectorAll("ion-icon");
+
+        console.log("bookmarkIcons", bookmarkIcons);
+        //
         movies.forEach((movie) => {
           if (movie.title === movieTitle) {
             if (movie.isBookmarked) {
               movie.isBookmarked = false;
-              event.target.closest(".bookmark").classList.remove("active");
+              // event.target.closest(".bookmark").classList.remove("active");
+              bookmarkIcons.forEach((icon) => {
+                icon.classList.toggle("hidden");
+              });
             } else {
               movie.isBookmarked = true;
-              event.target.closest(".bookmark").classList.add("active");
+              // event.target.closest(".bookmark").classList.add("active");
+              bookmarkIcons.forEach((icon) => {
+                icon.classList.toggle("hidden");
+              });
             }
             // console.log(movie.isBookmarked);
           }
@@ -646,39 +658,37 @@ function fillItem(movie) {
   }
   return `
   <figure class="movie-item">
-  <div class="bookmark ${movie.isBookmarked ? "active" : ""}">
-    <svg
-      width="12"
-      height="14"
-      xmlns="http://www.w3.org/2000/svg"
-      class="bookmark-icon"
-    >
-      <path
-        d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
-      />
-    </svg>
-  </div>
-  <img
-    src="${selectImg(movie)}"
-    alt="${movie.title}"
-    class="movie-img background"
-  />
-  <figcaption>
-    <ul class="movie-properties">
-      <li class="movie-property">
-        <p>${movie.year}</p>
-      </li>
-      <li class="movie-property">
-        <img src="assets/icon-category-movie.svg" alt="" />
-        <p>${movie.category}</p>
-      </li>
-      <li class="movie-property">
-        <p>${movie.rating}</p>
-      </li>
-    </ul>
-    <h3 class="heading-medium md">${movie.title}</h3>
-  </figcaption> 
-</figure>
+            <div class="bookmark">
+            <ion-icon class="bookmark--icon  ${
+              movie.isBookmarked ? "hidden" : ""
+            }" name="bookmark-outline"></ion-icon>
+            <ion-icon class="bookmark--icon  ${
+              movie.isBookmarked ? "" : "hidden"
+            }" name="bookmark" ></ion-icon>
+            </div>
+            <img
+              src="assets/thumbnails/the-great-lands/regular/large.jpg"
+              alt=""
+              class="movie-img background"
+            />
+            <figcaption>
+              <ul class="movie-properties">
+                <li class="movie-property">
+                  <img src="assets/icon-category-movie.svg" alt="" />
+                  <p>Movie</p>
+                </li>
+                <li class="movie-property">
+                  <img src="assets/icon-category-movie.svg" alt="" />
+                  <p>Movie</p>
+                </li>
+                <li class="movie-property">
+                  <img src="assets/icon-category-movie.svg" alt="" />
+                  <p>Movie</p>
+                </li>
+              </ul>
+              <h3 class="heading-medium md">Beyond Earth</h3>
+            </figcaption> 
+          </figure>
   `;
 }
 function fillItemSm(movie) {
@@ -705,40 +715,36 @@ function fillItemSm(movie) {
   }
   return `
   <figure class="movie-item sm">
-  <div class="bookmark ${movie.isBookmarked ? "active" : ""}">
-    <svg
-      width="12"
-      height="14"
-      xmlns="http://www.w3.org/2000/svg"
-      class="bookmark-icon"
-    >
-      <path
-        d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
-      />
-    </svg>
-  </div>
-  <img
-    src="${movie.thumbnail.regular.small}"
-    src="${selectImg(movie)}"
-    alt="${movie.title}"
-    class="movie-img"
-  />
-  <figcaption>
-    <ul class="movie-properties">
-      <li class="movie-property">
-        <p>${movie.year}</p>
-      </li>
-      <li class="movie-property">
-        <img src="assets/icon-category-movie.svg" alt="" />
-        <p>${movie.category}</p>
-      </li>
-      <li class="movie-property">
-        <p>${movie.rating}</p>
-      </li>
-    </ul>
-    <h3 class="heading-medium">${movie.title}</h3>
-  </figcaption>
-</figure>
+    <div class="bookmark">
+      <ion-icon class="bookmark--icon  ${
+        movie.isBookmarked ? "hidden" : ""
+      }" name="bookmark-outline"></ion-icon>
+      <ion-icon class="bookmark--icon  ${
+        movie.isBookmarked ? "" : "hidden"
+      }" name="bookmark" ></ion-icon>
+    </div>
+    <img
+      src="${movie.thumbnail.regular.small}"
+      src="${selectImg(movie)}"
+      alt="${movie.title}"
+      class="movie-img"
+    />
+    <figcaption>
+      <ul class="movie-properties">
+        <li class="movie-property">
+          <p>${movie.year}</p>
+        </li>
+        <li class="movie-property">
+          <img src="assets/icon-category-movie.svg" alt="" />
+          <p>${movie.category}</p>
+        </li>
+        <li class="movie-property">
+          <p>${movie.rating}</p>
+        </li>
+      </ul>
+      <h3 class="heading-medium">${movie.title}</h3>
+    </figcaption>
+  </figure>
     `;
 }
 
